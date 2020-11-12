3bfe21f086b87a9cf7c44d01743ade2ac72a5d0c
Dominik Piatek <dominik.piatek@ably.io>
2020-11-12 10:52:26 +0000

    Add watch for files at root

--- preview/config/webpack/development.js
+++ preview/config/webpack/development.js
@@ -1,5 +1,14 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const environment = require('./environment')
+const path = require("path");
+const environment = require("./environment");
 
-module.exports = environment.toWebpackConfig()
+environment.config.devServer.watchContentBase = true;
+environment.config.devServer.contentBase = [
+  path.join(__dirname, "../../app/views"),
+  path.join(__dirname, "../../../src"),
+  path.join(__dirname, "../../../lib"),
+  path.join(__dirname, "../../../core"),
+];
+
+module.exports = environment.toWebpackConfig();
