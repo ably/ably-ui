599ee7e5be12b1ad0691d4a60231ca1888262ae4
Dominik Piatek <dominik.piatek@ably.io>
2020-11-13 11:52:24 +0000

    Add reactRenderer as utility

--- src/core/scripts.js
+++ src/core/scripts.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 
+export { default as reactRenderer } from "./react-renderer";
+
 export function utilityFunction() {
-  console.log("Call utilityFunction");
+  console.log("Call utilityFunction!!");
 }
