0701acc8c1ae58d712ef90694f9b238c83b71c14
Dominik Piatek <dominik.piatek@ably.io>
2020-11-18 14:17:59 +0000

    Add a pre-release script
    This script is meant to allow releasing from a branch, to support preview apps.

---
+++ pre-release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# See https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
+set -euo pipefail
+
+if [ $# -lt 2 ]; then
+  echo $0: "Error: Please provide a valid development version, ie. ./release.sh 1.2.1 1"
+  exit 1
+fi
+
+VERSION=$1-dev.$2
+RUBY_VERSION=$1.dev.$2
+TAG=v$1-dev.$2
+
+echo "Building assets"
+node scripts/webpack-build.js
+
+echo "Tagging commit with $TAG"
+git tag -a $TAG -m "$TAG"
+
+echo "Updating version.rb file"
+echo -e "module AblyUi\n  VERSION = '$RUBY_VERSION'\nend" > ./lib/ably_ui/version.rb
+
+echo "Building the gem"
+gem build ably-ui.gemspec
+
+echo "Pushing the gem to the registry"
+gem push --key github \
+    --host https://rubygems.pkg.github.com/ably \
+    ably-ui-$RUBY_VERSION.gem
+
+echo "Publishing the npm package to the registry"
+yarn publish --no-git-tag-version --new-version $VERSION
+
+echo "Commiting version bump"
+git add package.json lib/ably_ui/version.rb && git commit -m "Bump version to $TAG"
+
+echo "Pushing tag to origin"
+# git push origin $TAG
