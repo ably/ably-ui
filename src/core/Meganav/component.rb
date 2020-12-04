dc6311456301a78babf1bdaba3ae80f87d9faac1
Dominik Piatek <dominik.piatek@ably.io>
2020-12-04 15:12:20 +0000

    Add Meganav component

--- src/core/Meganav/component.rb
+++ src/core/Meganav/component.rb
@@ -1,6 +1,16 @@
+require "json"
+
 module AblyUi
   module Core
     class Meganav < ViewComponent::Base
+      def panels
+        [
+          { label: "Platform", id: "platform-panel", class: "AblyUi::Core::PlatformPanel" },
+          { label: "Use Cases", id: "use-cases-panel", class: "AblyUi::Core::UseCasesPanel" },
+          { label: "Why Ably", id: "why-ably-panel", class: "AblyUi::Core::WhyAblyPanel" },
+          { label: "Developers", id: "developers-panel", class: "AblyUi::Core::DevelopersPanel" },
+        ]
+      end
     end
   end
 end
