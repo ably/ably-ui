68f3ab2732f300d97c2b86d324355e91404e2f3f
Dominik Piatek <dominik.piatek@ably.io>
2021-01-26 13:22:11 +0000

    Truncate accountName and prefferedEmail when signed in

--- src/core/MeganavItemsSignedIn/component.rb
+++ src/core/MeganavItemsSignedIn/component.rb
@@ -7,6 +7,14 @@ module AblyUi
         @theme_name = theme_name
         @session_data = session_data
       end
+
+      def account_name
+        truncate(@session_data[:accountName], length: 20)
+      end
+
+      def preferred_email
+        truncate(@session_data[:preferredEmail], length: 20)
+      end
     end
   end
 end
