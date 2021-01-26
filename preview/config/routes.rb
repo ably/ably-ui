b1d8efab2e59499268c8faff0b45662426d1ce87
Dominik Piatek <dominik.piatek@ably.io>
2021-01-26 11:52:11 +0000

    Create a separate route for meganav component
    Variants are handled with query params:
    ?vw=true triggers the View component partial instead of the React one
    ?signed-in=true triggers a dummy response with session data so the signed in view can be seen

--- preview/config/routes.rb
+++ preview/config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get '/globals', to: 'pages#globals'
   get '/foundations/typography', to: 'pages#typography'
 
+  get '/components/meganav', to: 'components#meganav'
+
+  get '/api/me', to: 'api/user_session#me'
+
   root 'pages#index'
 end
