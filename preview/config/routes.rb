e0e5e13996349cb3718373a7287d9be8bd2298ea
Dominik Piatek <dominik.piatek@ably.io>
2021-02-22 16:50:01 +0000

    Add Showcase component

--- preview/config/routes.rb
+++ preview/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/components/meganav', to: 'components#meganav'
   get '/components/footer', to: 'components#footer'
   get '/components/contact-footer', to: 'components#contact_footer'
+  get '/components/showcase', to: 'components#showcase'
 
   get '/api/me', to: 'api/user_session#me'
   get '/api/blog', to: 'api/blog#recent_blog_posts'
