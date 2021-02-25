55b511f1f11d5a13f26a0f683c58493c235e857b
Dominik Piatek <dominik.piatek@ably.io>
2021-02-25 12:35:49 +0000

    Move ably stack path into SharedAssets module

--- src/core/MeganavContentPlatform/component.rb
+++ src/core/MeganavContentPlatform/component.rb
@@ -1,9 +1,7 @@
 module AblyUi
   module Core
     class MeganavContentPlatform < ViewComponent::Base
-      def ably_stack_path
-        asset_path 'ably_ui/core/images/ably-stack.svg'
-      end
+      include AblyUi::Core::SharedAssets
     end
   end
 end
