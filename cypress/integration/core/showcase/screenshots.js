2178f0eb2fb3497ac32eb8631641606961be9a19
Dominik Piatek <dominik.piatek@ably.io>
2021-02-26 17:19:53 +0000

    Swap snapshot tests in favour of a screenshot test
    The showcase component contains a random ID which will make snapshot test fail every time.

---
+++ cypress/integration/core/showcase/screenshots.js
@@ -0,0 +1,18 @@
+import { fixSnapshotSpec } from "../../../support";
+
+beforeEach(fixSnapshotSpec(__filename));
+
+describe("Showcase", () => {
+  beforeEach(() => {
+    cy.viewport("macbook-16");
+    cy.visit("/components/showcase?framework=vw");
+  });
+
+  it("looks as per design in a wide container", () => {
+    cy.get("[data-id='showcase']").first().toMatchImageSnapshot();
+  });
+
+  it("looks as per design in a narrow container", () => {
+    cy.get("[data-id='showcase']").last().toMatchImageSnapshot();
+  });
+});
