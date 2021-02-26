76a5c163869f3e306c8d5be4025373dfa9fe77e7
Dominik Piatek <dominik.piatek@ably.io>
2021-02-26 16:21:23 +0000

    Address feedback

--- src/core/Footer/component.jsx
+++ src/core/Footer/component.jsx
@@ -5,203 +5,274 @@ import "./component.css";
 
 export default function Footer({ paths }) {
   return (
-    <section className="ui-footer font-light pt-32" data-id="footer">
-      <div className="ui-footer-content ui-grid-gap sm:grid-cols-2 md:grid-cols-6">
-        <div className="sm:col-span-full md:col-span-2">
+    <footer className="bg-light-grey font-sans antialiased" data-id="footer">
+      <div className="max-w-screen-xl mx-auto p-24 md:px-32 lg:p-32 xl:p-64 ui-grid-gap grid grid-cols-6">
+        <div className="col-span-full md:col-span-2">
           <div className="flex flex-row p-menu-row-snug">
             <img className="mr-24 -mt-16" src={paths.ablyStack} alt="Ably homepage" />
-            <h5 className="text-overline2 col-span-full font-bold uppercase">The Ably Platform</h5>
+            <h2 className="text-overline2 col-span-full font-medium uppercase tracking-widen-0.1">The Ably Platform</h2>
           </div>
 
-          <div className="p-3 py-16 font-medium p-menu-row-snug">
-            Easily power any realtime experience in your application via a simple API that handles everything realtime.
+          <div className="grid grid-cols-4">
+            <p className="text-p3 py-16 font-medium p-menu-row-snug col-span-3">
+              Easily power any realtime experience in your application via a simple API that handles everything realtime.
+            </p>
           </div>
 
-          <ul className="grid grid-cols-2 lg:grid-cols-1">
+          <ul className="grid gap-x-8 sm:gap-x-16 md:gap-x-24 xl:gap-x-32 sm:grid-cols-2 md:grid-cols-1">
             <li className="p-menu-row-snug">
-              <a href="/pub-sub-messaging">Pub/sub messaging</a>
+              <a href="/pub-sub-messaging" className="ui-footer-menu-row-link">
+                Pub/sub messaging
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/push-notifications">Push notifications</a>
+              <a href="/push-notifications" className="ui-footer-menu-row-link">
+                Push notifications
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/integrations">Third-party integrations</a>
+              <a href="/integrations" className="ui-footer-menu-row-link">
+                Third-party integrations
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/protocols">Multiple protocol messaging</a>
+              <a href="/protocols" className="ui-footer-menu-row-link">
+                Multiple protocol messaging
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/hub">Streaming data sources</a>
+              <a href="/hub" className="ui-footer-menu-row-link">
+                Streaming data sources
+              </a>
             </li>
           </ul>
         </div>
 
-        <div>
-          <h5 className="text-overline2  p-menu-row-snug font-bold uppercase">Ably is for</h5>
+        <div className="col-span-full sm:col-span-3 md:col-span-1">
+          <h2 className="ui-footer-col-title">Ably is for</h2>
           <ul>
             <li className="p-menu-row-snug">
-              <a href="/solutions/edtech">EdTech</a>
+              <a href="/solutions/edtech" className="ui-footer-menu-row-link">
+                EdTech
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/solutions/automotive-logistics-and-mobility">Automotive, Logistics, &amp; Mobility</a>
+              <a href="/solutions/automotive-logistics-and-mobility" className="ui-footer-menu-row-link">
+                Automotive, Logistics, &amp; Mobility
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/solutions/b2b-platforms">B2B Platforms</a>
+              <a href="/solutions/b2b-platforms" className="ui-footer-menu-row-link">
+                B2B Platforms
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/solutions/healthcare">Healthcare</a>
+              <a href="/solutions/healthcare" className="ui-footer-menu-row-link">
+                Healthcare
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/hub">Streaming data sources</a>
+              <a href="/hub" className="ui-footer-menu-row-link">
+                Streaming data sources
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/solutions/ecommerce-and-retail">eCommerce &amp; Retail</a>
+              <a href="/solutions/ecommerce-and-retail" className="ui-footer-menu-row-link">
+                eCommerce &amp; Retail
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/solutions/sports-and-media">Sports &amp; Media</a>
+              <a href="/solutions/sports-and-media" className="ui-footer-menu-row-link">
+                Sports &amp; Media
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/solutions/gaming">Gaming</a>
+              <a href="/solutions/gaming" className="ui-footer-menu-row-link">
+                Gaming
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/solutions/iot-and-connected-devices">IoT &amp; Connected Devices</a>
+              <a href="/solutions/iot-and-connected-devices" className="ui-footer-menu-row-link">
+                IoT &amp; Connected Devices
+              </a>
             </li>
           </ul>
         </div>
 
-        <div>
-          <h5 className="text-overline2 p-menu-row-snug font-bold uppercase">Developers</h5>
+        <div className="col-span-full sm:col-span-3 md:col-span-1">
+          <h2 className="ui-footer-col-title">Developers</h2>
           <ul>
             <li className="p-menu-row-snug">
-              <a href="/documentation/quick-start-guide">Start in 5 minutes</a>
+              <a href="/documentation/quick-start-guide" className="ui-footer-menu-row-link">
+                Start in 5 minutes
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/documentation/">Documentation</a>
+              <a href="/documentation/" className="ui-footer-menu-row-link">
+                Documentation
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/tutorials">Tutorials</a>
+              <a href="/tutorials" className="ui-footer-menu-row-link">
+                Tutorials
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="https://changelog.ably.com/">Changelog</a>
+              <a href="https://changelog.ably.com/" className="ui-footer-menu-row-link">
+                Changelog
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/support">Support &amp; FAQs</a>
+              <a href="/support" className="ui-footer-menu-row-link">
+                Support &amp; FAQs
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/download">SDKs</a>
+              <a href="/download" className="ui-footer-menu-row-link">
+                SDKs
+              </a>
             </li>
-            <li className="p-menu-row-snug flex">
-              <a className="flex-none pr-8" href="https://status.ably.com/">
+            <li className="p-menu-row-snug flex items-center">
+              <a className="pr-8 ui-footer-menu-row-link" href="https://status.ably.com/">
                 System status
               </a>
-              <iframe className="h-24 mt-4" src="https://status.ably.io/embed/icon" allowtransparency="true" frameBorder="0" scrolling="no"></iframe>
+              <iframe className="w-24 h-24 mt-4" src="https://status.ably.io/embed/icon" allowtransparency="true" frameBorder="0" scrolling="no"></iframe>
             </li>
           </ul>
         </div>
 
-        <div>
-          <h5 className="text-overline2 p-menu-row-snug font-bold uppercase">WHY ABLY</h5>
+        <div className="col-span-full sm:col-span-3 md:col-span-1">
+          <h2 className="ui-footer-col-title">WHY ABLY</h2>
           <ul>
             <li className="p-menu-row-snug">
-              <a href="/customers">Customer Stories</a>
+              <a href="/customers" className="ui-footer-menu-row-link">
+                Customer Stories
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/four-pillars-of-dependability">Four Pillars of Dependability</a>
+              <a href="/four-pillars-of-dependability" className="ui-footer-menu-row-link">
+                Four Pillars of Dependability
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/compare">Compare our tech</a>
+              <a href="/compare" className="ui-footer-menu-row-link">
+                Compare our tech
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/protocols">Multi protocol support</a>
+              <a href="/protocols" className="ui-footer-menu-row-link">
+                Multi protocol support
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/integrations">Third-party integrations</a>
+              <a href="/integrations" className="ui-footer-menu-row-link">
+                Third-party integrations
+              </a>
             </li>
           </ul>
         </div>
 
-        <div>
-          <h5 className="text-overline2 p-menu-row-snug font-bold uppercase">ABOUT</h5>
+        <div className="col-span-full sm:col-span-3 md:col-span-1">
+          <h2 className="ui-footer-col-title">ABOUT</h2>
           <ul>
             <li className="p-menu-row-snug">
-              <a href="/about">About Ably</a>
+              <a href="/about" className="ui-footer-menu-row-link">
+                About Ably
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/pricing">Pricing</a>
+              <a href="/pricing" className="ui-footer-menu-row-link">
+                Pricing
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/blog">Blog</a>
+              <a href="/blog" className="ui-footer-menu-row-link">
+                Blog
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/enterprise">Enterprise</a>
+              <a href="/enterprise" className="ui-footer-menu-row-link">
+                Enterprise
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/careers">Careers</a>
+              <a href="/careers" className="ui-footer-menu-row-link">
+                Careers
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/open-policy">Open protocol policy</a>
+              <a href="/open-policy" className="ui-footer-menu-row-link">
+                Open protocol policy
+              </a>
             </li>
             <li className="p-menu-row-snug">
-              <a href="/contact">Contact us</a>
+              <a href="/contact" className="ui-footer-menu-row-link">
+                Contact us
+              </a>
             </li>
           </ul>
         </div>
       </div>
 
-      <hr className="border-t-2 mx-24" />
+      <div className="max-w-screen-xl px-24 md:px-32 xl:px-64 mx-auto">
+        <hr className="border-t-1 border-mid-grey" />
+      </div>
 
-      <div className="ui-footer-content ui-grid-gap sm:grid-cols-1 md:grid-cols-2">
-        <div className="flex flex-col flex-auto pb-32">
-          <div className="ui-footer-legals text-gui-default">
-            <a href="/privacy" className="pr-24">
+      <div className="max-w-screen-xl mx-auto p-24 md:px-32 lg:p-32 xl:px-64 grid grid-cols-2">
+        <div className="flex flex-col flex-auto pb-40 ml-12 col-span-full md:col-span-1">
+          <div className="ui-footer-legals">
+            <a href="/privacy" className="pr-24 ui-footer-link">
               Cookies
             </a>
-            <a href="/legals" className="pr-24">
+            <a href="/legals" className="pr-24 ui-footer-link">
               Legals
             </a>
-            <a href="/data-protection">Data Protection</a>
+            <a href="/data-protection" className="ui-footer-link">
+              Data Protection
+            </a>
           </div>
 
-          <div className="pt-32 flex align-middle">
-            <a className="pr-24 text-cool-black hover:text-icon-twitter" href="https://twitter.com/ablyrealtime">
-              <svg className="w-24 h-24" id="ui-footer-icon-twitter">
+          <div className="pt-32 flex items-center">
+            <a className="h-24 pr-24 text-cool-black hover:text-icon-twitter" href="https://twitter.com/ablyrealtime">
+              <svg className="w-24 h-24">
                 <use xlinkHref="#sprite-twitter"></use>
               </svg>
             </a>
-            <a className="pr-24 text-cool-black hover:text-icon-linkedin" href="https://www.linkedin.com/company/ably-realtime">
-              <svg className="w-24 h-24" id="ui-footer-icon-linkedin">
+            <a className="h-24 pr-24 text-cool-black hover:text-icon-linkedin" href="https://www.linkedin.com/company/ably-realtime">
+              <svg className="w-24 h-24">
                 <use xlinkHref="#sprite-linkedin"></use>
               </svg>
             </a>
-            <a className="pr-24 text-cool-black hover:text-icon-github" href="https://github.com/ably/">
-              <svg className="w-24 h-24" id="ui-footer-icon-github">
+            <a className="h-24 pr-24 text-cool-black hover:text-icon-github" href="https://github.com/ably/">
+              <svg className="w-24 h-24">
                 <use xlinkHref="#sprite-github"></use>
               </svg>
             </a>
+            <div className="border-l-2 h-40"></div>
             <a
-              className="flex flex-row border-l-2 pl-24 text-cool-black hover:text-icon-glassdoor"
+              className="flex flex-row pl-24 text-cool-black hover:text-icon-glassdoor"
               href="https://www.glassdoor.co.uk/Overview/Working-at-Ably-EI_IE2184188.11,15.htm"
             >
-              <svg className="w-24 h-24" id="ui-footer-icon-glassdoor">
+              <svg className="w-24 h-24">
                 <use xlinkHref="#sprite-glassdoor"></use>
               </svg>
             </a>
-            <div className="text-menu3 pl-16 leading-5 -mt-8">
-              <strong className="block">Were hiring! </strong>
-              <a href="https://www.glassdoor.co.uk/Overview/Working-at-Ably-EI_IE2184188.11,15.htm" className="text-gui-default">
-                learn
+            <div className="pl-16 text-menu3 font-light">
+              <strong className="block font-medium">Were hiring!</strong>
+              <a href="https://www.glassdoor.co.uk/Overview/Working-at-Ably-EI_IE2184188.11,15.htm" className="ui-footer-link">
+                Learn more
               </a>
-              &nbsp;more at Glass Door
+              &nbsp;at Glass Door
             </div>
           </div>
         </div>
 
-        <div className="md:text-right sm:col-span-full md:col-span-1">
+        <div className="md:text-right col-span-full md:col-span-1 ml-12">
           <img className="mr-24 w-96 h-96" src={paths.rocketList} alt="Rocket List 2021" />
-          <img className="mr-24 w-96 h-96" src={paths.flexibleCompanies} alt="Flexible Companies 2021" />
+          <img className="w-96 h-96" src={paths.flexibleCompanies} alt="Flexible Companies 2021" />
         </div>
       </div>
-    </section>
+    </footer>
   );
 }
 
