b493d25cd2ef7e123ff387a6f98944a017064d7e
Bruce Thomas <burce.thomas@ably.com>
2021-04-20 12:03:08 +0100

    Consolidate toggle widget method

---
+++ src/core/hubspot-chat-toggle.js
@@ -0,0 +1,67 @@
+import { queryId } from "./dom-query";
+
+/*
+    A method to enable/disable a CTA that
+    is used to open the HubSpot chat widget
+
+    If the Chat widget is unavailable this script
+    will apply a disabled propery and change
+    the text on the button, to warn the user.
+
+    Params:
+    - dataId        the parent container data-id
+    - textEnabled   label for an active button
+    - textDisable   label for an active button
+
+*/
+
+function enableBtn(el, text) {
+  el.disabled = false;
+  el.innerText = text || "Start a live chat";
+}
+
+function disableBtn(el, text) {
+  el.disabled = true;
+  el.innerText = text || "Live chat unavailable";
+}
+
+const WAIT_BETWEEN_RETRIES_MS = 100;
+let MAX_RETRY_COUNT = 30;
+
+
+export default function toggleChatWidget(params = {}) {
+  const {dataId, textEnabled, textDisabled} = params;
+  console.log("init", params);
+  const container = queryId(dataId);
+
+  if(!dataId || !container) return;
+  const trigger = queryId("open-chat-widget", container);
+
+  let clickHandler;
+
+  const waitForScript = (delay) => {
+    const widget = window?.HubSpotConversations?.widget;
+
+    clickHandler = (e) => {
+      e.preventDefault();
+      widget.open();
+    };
+
+    if (widget) {
+      trigger.addEventListener("click", clickHandler);
+      enableBtn(trigger, textEnabled);
+    } else if (--MAX_RETRY_COUNT) {
+      setTimeout(() => waitForScript(WAIT_BETWEEN_RETRIES_MS), delay);
+    }
+
+    trigger.style.visibility = "visible";
+  };
+
+  disableBtn(trigger, textDisabled);
+  waitForScript(0);
+
+  return () => {
+    disableBtn(trigger, textDisabled);
+    trigger.removeEventListener("click", clickHandler);
+  };
+}
