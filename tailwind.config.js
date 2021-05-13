65de94b9e9ec78e049179b6b005b0871e8e8f774
Dominik Piatek <dominik.piatek@ably.io>
2021-05-13 11:19:28 +0100

    Include any class that starts with ui-
    This to ensure any dynamic classes are included

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     options: {
       safelist: {
         standard: ["w-1/2", "w-1/3", "w-1/4", "w-1/5", "w-1/6"],
-        greedy: [/^hljs.*/],
+        greedy: [/^hljs.*/, /^ui-.*/],
       },
     },
   },
