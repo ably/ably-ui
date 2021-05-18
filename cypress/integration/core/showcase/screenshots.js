5a55eb701197427363ddca4a7a7d68ce513f7200
Dominik Piatek <dominik.piatek@ably.io>
2021-05-18 10:05:43 +0100

    Update spec

--- cypress/integration/core/showcase/screenshots.js
+++ cypress/integration/core/showcase/screenshots.js
@@ -2,17 +2,19 @@ import { fixSnapshotSpec } from "../../../support";
 
 beforeEach(fixSnapshotSpec(__filename));
 
-describe.skip("Showcase", () => {
+describe("Showcase", () => {
   beforeEach(() => {
     cy.viewport("macbook-16");
     cy.visit("/components/showcase?framework=vw");
   });
 
   it("looks as per design in a wide container", () => {
-    cy.get("[data-id='showcase']").first().toMatchImageSnapshot();
+    cy.get("[data-id='showcase']").first().should("be.visible");
+    cy.percySnapshot();
   });
 
   it("looks as per design in a narrow container", () => {
-    cy.get("[data-id='showcase']").eq(1).toMatchImageSnapshot();
+    cy.get("[data-id='showcase']").eq(1).should("be.visible");
+    cy.percySnapshot();
   });
 });
