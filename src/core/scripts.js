231fec9b0b62a31ffc87491c881897591f464361
Dominik Piatek <dominik.piatek@ably.io>
2021-06-08 15:04:01 +0100

    Remove react-renderer util from core module

--- src/core/scripts.js
+++ src/core/scripts.js
@@ -2,8 +2,7 @@ import "array-flat-polyfill";
 
 import "./styles.css";
 
-export { default as reactRenderer } from "./react-renderer";
-export { default as loadSprites } from "./load-sprites";
+export { default as loadSprites } from "./util/load-sprites";
 export * from "./remote-data-store";
 export * from "./remote-blogs-posts";
 export * from "./remote-session-data";
