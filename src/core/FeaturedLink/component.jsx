341146da15c05e19a9ef58d524417adad38b6000
Dominik Piatek <dominik.piatek@ably.io>
2021-06-18 19:44:27 +0100

    Add styling options to FeaturedLink component

--- src/core/FeaturedLink/component.jsx
+++ src/core/FeaturedLink/component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import T from "prop-types";
 
-const FeaturedLink = ({ url, children }) => (
+const FeaturedLink = ({ url, textColor = "text-gui-default", textSize = "text-menu3", textWeight = "font-medium", children }) => (
   <a
     href={url}
-    className={`${textSize} ${textWeight} ${textColor} flex items-center hover:text-gui-hover focus:text-gui-focus focus:outline-gui-focus py-8 block group`}
+    className={`${textSize} ${textColor} ${textWeight} flex items-center hover:text-gui-hover focus:text-gui-focus focus:outline-gui-focus py-8 block group`}
   >
     {children}
     <svg className="w-12 h-12 transform -rotate-90 align-top ui-icon-dark-grey group-hover:icon-gui-hover group-focus:icon-gui-focus ml-4">
@@ -16,6 +16,9 @@ const FeaturedLink = ({ url, children }) => (
 FeaturedLink.propTypes = {
   url: T.string,
   children: T.node,
+  textSize: T.string,
+  textColor: T.string,
+  textWeight: T.string,
 };
 
 export default FeaturedLink;
