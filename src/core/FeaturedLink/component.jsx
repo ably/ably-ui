178ae87e7d42dda871f70b36f6f87e5a064c6f98
Dominik Piatek <dominik.piatek@ably.io>
2021-06-18 12:26:52 +0100

    Fix FeaturedLink icon not being aligned

--- src/core/FeaturedLink/component.jsx
+++ src/core/FeaturedLink/component.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import T from "prop-types";
 
 const FeaturedLink = ({ url, children }) => (
-  <a href={url} className="text-menu3 font-medium text-gui-default hover:text-gui-hover focus:text-gui-focus focus:outline-gui-focus py-8 block group">
+  <a
+    href={url}
+    className="text-menu3 font-medium text-gui-default flex items-center hover:text-gui-hover focus:text-gui-focus focus:outline-gui-focus py-8 block group"
+  >
     {children}
     <svg className="w-12 h-12 transform -rotate-90 align-top ui-icon-dark-grey group-hover:icon-gui-hover group-focus:icon-gui-focus ml-4">
       <use xlinkHref="#sprite-disclosure-arrow" />
