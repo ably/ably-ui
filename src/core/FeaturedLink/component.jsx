2707dab52e7cc62b18b03240b14ccb4d299dd935
Dominik Piatek <dominik.piatek@ably.io>
2021-06-23 16:19:39 +0100

    Update FeaturedLink options
    This adjusts the options of the FeaturedLink to the ones we require right now.
    
    Also updates the alignment of the chevron & it's color.

--- src/core/FeaturedLink/component.jsx
+++ src/core/FeaturedLink/component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import T from "prop-types";
 
-const FeaturedLink = ({ url, textColor = "text-gui-default", textSize = "text-menu3", textWeight = "font-medium", children }) => (
+const FeaturedLink = ({ url, textSize = "text-menu3", iconColor = "ui-icon-cool-black", flush = false, children }) => (
   <a
     href={url}
-    className={`${textSize} ${textColor} ${textWeight} flex items-center hover:text-gui-hover focus:text-gui-focus focus:outline-gui-focus py-8 block group`}
+    className={`${textSize} text-gui-default font-medium flex items-baseline hover:text-gui-hover focus:text-gui-focus focus:outline-gui-focus ${
+      flush ? "" : "py-8"
+    } block group`}
   >
     {children}
-    <svg className="w-12 h-12 transform -rotate-90 align-top ui-icon-dark-grey group-hover:icon-gui-hover group-focus:icon-gui-focus ml-4">
+    <svg className={`w-12 h-12 transform -rotate-90 align-top ${iconColor} group-hover:icon-gui-hover group-focus:icon-gui-focus ml-4`}>
       <use xlinkHref="#sprite-disclosure-arrow" />
     </svg>
   </a>
@@ -17,8 +19,8 @@ FeaturedLink.propTypes = {
   url: T.string,
   children: T.node,
   textSize: T.string,
-  textColor: T.string,
-  textWeight: T.string,
+  iconColor: T.string,
+  flush: T.bool,
 };
 
 export default FeaturedLink;
