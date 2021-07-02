92785ac0ea3c0f7933c46422078670687ef8f0cf
Bruce Thomas <burce.thomas@ably.com>
2021-07-02 15:55:41 +0100

    Remove padding and overflow from Code component

--- src/core/Code/component.jsx
+++ src/core/Code/component.jsx
@@ -8,7 +8,7 @@ const Code = ({ language, snippet }) => {
   const HTMLraw = highlight(language, `${snippet}`.trim());
   const className = `font-mono language-${language}`;
   return (
-    <div className="hljs p-32 overflow-auto" data-id="code">
+    <div className="hljs" data-id="code">
       <pre lang={language}>
         <code className={className} dangerouslySetInnerHTML={{ __html: HTMLraw }} />
       </pre>
