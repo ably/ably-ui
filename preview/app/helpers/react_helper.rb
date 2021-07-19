670f19ebe6196ae09622551158973d62e78607d9
Dominik Piatek <dominik.piatek@ably.io>
2021-07-19 14:30:53 +0100

    Update react_helper with wrapper_css option
    This option let's the caller add css classes to the wrapping div.

--- preview/app/helpers/react_helper.rb
+++ preview/app/helpers/react_helper.rb
@@ -1,12 +1,23 @@
 module ReactHelper
   def react_component(name, props = {}, options = {})
-    format_keys = (options || {}).fetch(:format_keys, true)
-    props = props.deep_transform_keys { |key| String(key).camelize(:lower) } if format_keys
+    format_keys = options.fetch(:format_keys, true)
+    wrapper_css = options.fetch(:wrapper_css, '')
+    props =
+      props.deep_transform_keys do |key|
+        String(key).camelize(:lower)
+      end if format_keys
+
     # Pass name as dash-case, like the file or HTML attributes, but keep the convention
     # of upper camelcase for React components
     camelized_name = name.underscore.camelcase
-    classnames = options[:classnames] || '';
-
-    content_tag(:div, "", class: classnames, data: { react: camelized_name, react_props: props })
+    content_tag(
+      :div,
+      '',
+      data: {
+        react: camelized_name,
+        react_props: props
+      },
+      class: wrapper_css
+    )
   end
 end
