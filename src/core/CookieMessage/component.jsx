badf0639e927f102f5bd99ef18d3466594813c71
Bruce Thomas <burce.thomas@ably.com>
2021-07-22 10:44:29 +0100

    Cookie Message Component

--- src/core/CookieMessage/component.jsx
+++ src/core/CookieMessage/component.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import T from "prop-types";
 import Cookie from "js-cookie";
 
 const COOKIE_EXPIRY = 365;
 
-const cookieId = "footer-cookie-message";
-const hasBeenClosedBefore = Cookie.get(cookieId);
+export default function CookieMessage(props) {
+  const { cookieId } = props;
+  const ref = useRef(null);
+  const [hideCookieMessage, setHideCookieMessage] = useState(true);
+  const handleClose = () => {
+    Cookie.set(cookieId, "1", { expires: COOKIE_EXPIRY });
 
-function handleClose() {
-  const { current } = this;
-  const deleteSelf = () => current.parentNode.remove(current);
+    ref.current.classList.add("bottom-1", "opacity-0");
+    setTimeout(() => setHideCookieMessage(true), 500);
+  };
 
-  current.classList.add("ui-cookie-message-vanish");
-  Cookie.set(cookieId, cookieId, { expires: COOKIE_EXPIRY });
-  setTimeout(deleteSelf, 450);
-}
+  useEffect(() => {
+    const isCookieSet = Cookie.get(cookieId) ? true : false;
+    setHideCookieMessage(isCookieSet);
+  }, []);
 
-export default function CookieMessage() {
-  if (hasBeenClosedBefore) return null;
-
-  const ref = React.createRef();
+  // Presume the message is hidden by default
+  if (hideCookieMessage) return null;
 
   return (
-    <div className="ui-cookie-message-cyan" ref={ref}>
+    <div className="ui-cookie-message-container" ref={ref}>
       <span className="text-p2 text-cool-black pr-32">
         We use cookies to improve your experience and for marketing, laid out in our{" "}
         <a href="/privacy" className="underline">
@@ -29,9 +32,13 @@ export default function CookieMessage() {
         </a>
         .
       </span>
-      <button className="ui-btn-secondary mt-12 sm:mt-0 whitespace-nowrap" onClick={handleClose.bind(ref)}>
+      <button className="ui-btn-secondary mt-12 sm:mt-0 whitespace-nowrap" onClick={handleClose}>
         Accept and close
       </button>
     </div>
   );
 }
+
+CookieMessage.propTypes = {
+  cookieId: T.string,
+};
