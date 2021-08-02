fc0a945a4bfcfa703ec7b55dcb409d56e367f09c
Dominik Piatek <dominik.piatek@ably.io>
2021-08-02 11:07:39 +0100

    Removed legacy routes

--- preview/config/routes.rb
+++ preview/config/routes.rb
@@ -2,8 +2,6 @@ Rails
   .application
   .routes
   .draw do
-    get '/globals', to: 'pages#globals'
-    get '/foundations/typography', to: 'pages#typography'
 
     get '/components/meganav', to: 'components#meganav'
     get '/components/footer', to: 'components#footer'
