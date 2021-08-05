a1f91cf4505cb27366ba71fd9ba70c1ffe30d443
Dominik Piatek <dominik.piatek@ably.io>
2021-08-05 11:51:57 +0100

    Add option to Meganav for a custom login link

--- src/core/MeganavItemsMobile/component.rb
+++ src/core/MeganavItemsMobile/component.rb
@@ -2,10 +2,12 @@ module AblyUi
   module Core
     class MeganavItemsMobile < ViewComponent::Base
       include MeganavConfig
+      attr_reader :options
 
-      def initialize(session_data:, theme_name:)
+      def initialize(session_data:, theme_name:, options: {})
         @theme_name = theme_name
         @session_data = session_data
+        @options = options
       end
     end
   end
