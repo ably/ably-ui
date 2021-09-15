3595f6cb8d947216ddb4cb7df8c41893931beb0f
Arti Mathanda <arti.mathanda@ably.com>
2021-09-15 16:09:35 +0100

    Extends FeatureFooter to accept more props

--- src/core/FeatureFooter/component.rb
+++ src/core/FeatureFooter/component.rb
@@ -3,18 +3,27 @@ module AblyUi
     class FeatureFooter < ViewComponent::Base
       include AblyUi::Core::Util
 
-      attr_reader :url_base, :image_url, :cta_text, :text
+      attr_reader :url_base,
+                  :image_url,
+                  :title,
+                  :subtitle,
+                  :live_chat_label,
+                  :live_chat_disabled_label
 
       def initialize(
         url_base: AblyUi::Core::Util::DEFAULT_URL_BASE,
         image_url: nil,
-        cta_text: 'Talk to our technical team',
-        text: "If you're having technical or account issues just get in touch."
+        title: 'Talk to our technical team',
+        subtitle: "If you're having technical or account issues just get in touch.",
+        live_chat_label: 'Start a live chat',
+        live_chat_disabled_label: 'Live chat unavailable'
       )
         @url_base = url_base
         @image_url = image_url
-        @cta_text = cta_text
-        @text = text
+        @title = title
+        @subtitle = subtitle
+        @live_chat_label = live_chat_label
+        @live_chat_disabled_label = live_chat_disabled_label
       end
     end
   end
