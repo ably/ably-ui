14ddf4dcdc7d623228fa2d21ff137f5576437a40
Dominik Piatek <dominik.piatek@ably.io>
2021-09-20 15:37:54 +0100

    Add tailwind config extender

---
+++ tailwind.extend.js
@@ -0,0 +1,27 @@
+const ablyUIConfig = require("./tailwind.config");
+const path = require("path");
+
+const ablyUiConfigPath = path.dirname(require.resolve("./tailwind.config.js"));
+
+const ablyUItailwindConfig = (extend) => {
+  // Create absolute paths to templates in AblyUI
+  const addPurgeContentPaths = () => {
+    const paths = ["*.html.erb", "*.jsx", "*.js", "*.json"].map((fileGlob) =>
+      path.join(ablyUiConfigPath, "src", "**", fileGlob)
+    );
+
+    return paths;
+  };
+
+  const configWithPlugin = {
+    ...ablyUIConfig,
+    purge: {
+      content: addPurgeContentPaths(),
+      options: ablyUIConfig.purge.options,
+    },
+  };
+
+  return extend(configWithPlugin);
+};
+
+module.exports = ablyUItailwindConfig;
