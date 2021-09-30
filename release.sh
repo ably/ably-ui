14464fe560bca27b617fb8cfcfbf4b2899ad7e11
Denis Sellu <denis.sellu@ably.com>
2021-09-30 11:58:01 +0100

    WIP test 11

--- release.sh
+++ release.sh
@@ -2,49 +2,10 @@
 
 # See https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 set -euo pipefail
-echo $1
 env
 
 VERSION=$1
 TAG=v$1
-
-echo "==============================="
-echo "VERSION---"
-echo $VERSION
-echo "TAG---"
-echo $TAG
-echo "$RUBYGEMS_API_KEY=-----------"
-echo $RUBYGEMS_API_KEY
-echo "==============================="
-
-if [ $# -ne 1 ]; then
-  echo $0: "Error: Please provide a valid semver version, ie. ./release.sh 1.2.1"
-  exit 1
-fi
-
-BRANCH=$(git branch --show-current)
-
-if [[ $BRANCH != "main" ]]; then
-  echo $0: "Error: Releases can only be made from the main branch"
-  exit 1
-fi
-
-COMMITS_AHEAD=$(git rev-list main...origin/main --count)
-
-if [ $COMMITS_AHEAD -gt 0 ]; then
-  echo $0: "Error: branch origin/main is ahead of your local main"
-  exit 1
-fi
-
-if [[ `git status --porcelain --untracked-files=no` ]]; then
-  echo $0: "Error: you have uncommited changes. A package is created from the filesystem, not git state so it's important to not have uncommited changes."
-  exit 1
-fi
-
-if git rev-parse "${TAG}" >/dev/null 2>&1; then
-  echo $0: "Error: ${TAG} already exists"
-  exit 1
-fi
 
 echo "Install packages, making sure they are up to date"
 yarn --frozen-lockfile
@@ -56,7 +17,7 @@ echo "Build library"
 NODE_ENV=production node scripts/build.js
 
 echo "Update version.rb file"
-echo -e "module AblyUi\n  VERSION = '${VERSION}'\nend" > ./lib/ably_ui/version.rb
+echo -e "module AblyUi\n  VERSION = '$VERSION'\nend" > ./lib/ably_ui/version.rb
 
 echo "Build the gem"
 gem build ably-ui.gemspec
@@ -67,8 +28,7 @@ mkdir -p ~/.gem
 
 cat << EOF > ~/.gem/credentials
 ---
-:github: Bearer ${GITHUB_TOKEN}
-:rubygems_api_key: ${GITHUB_TOKEN}
+:github: Bearer ${GITHUB_REGISTRY_TOKEN}
 EOF
 
 chmod 0600 ~/.gem/credentials
@@ -77,14 +37,14 @@ set -x
 echo "Push the gem to the registry"
 gem push --key github \
     --host https://rubygems.pkg.github.com/ably \
-    ably-ui-${VERSION}.gem
+    ably-ui-$VERSION.gem
 
 echo "Update Gemfile.lock"
 bundle config unset deployment
 bundle
 
 echo "Remove local gem artifact"
-rm ably-ui-${VERSION}.gem
+rm ably-ui-$VERSION.gem
 
 echo "Publish the npm package to the registry"
 yarn publish --no-git-tag-version --new-version $VERSION
