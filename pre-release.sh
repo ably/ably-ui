235456c7a182a0f394f1aaeb5a8c6bd7fcf69a78
Bruce Thomas <burce.thomas@ably.com>
2021-10-21 16:04:45 +0100

    wip - prerelease

--- pre-release.sh
+++ pre-release.sh
@@ -3,7 +3,7 @@
 # See https://coderwall.com/p/fkfaqq/safer-bash-scripts-with-set-euxo-pipefail
 set -euo pipefail
 
-echo "Fetching remote for up to date commit history"
+echo "> Fetching remote for up to date commit history"
 git fetch
 
 BRANCH=$(git branch --show-current)
@@ -19,11 +19,11 @@ if [[ `git status --porcelain --untracked-files=no` ]]; then
   exit 1
 fi
 
-echo "Save short SHA for package name"
+echo "> Save short SHA for package name"
 PACKAGE_SUFFIX=$(git rev-parse --short HEAD)
-echo "SHA is ${PACKAGE_SUFFIX}"
+echo "> SHA is ${PACKAGE_SUFFIX}"
 
-echo "Read the current semver from package.json, ignoring pre-release versions"
+echo "> Read the current semver from package.json, ignoring pre-release versions"
 ABLY_UI_VERSION=$(node -e "const p = require('./package.json'); console.log(p.version.split('-dev')[0]);")
 
 # Note the . and - before "dev" - this is due to the differences between gems and npm in what they consider a pre-release version
@@ -36,56 +36,56 @@ if git rev-parse "${TAG}" >/dev/null 2>&1; then
   exit 1
 fi
 
-echo "Install packages, making sure they are up to date"
+echo "> Install packages, making sure they are up to date"
 yarn --frozen-lockfile
 bundle config set --local frozen true
 bundle
 bundle config set --local frozen false
 
-echo "Build library"
+echo "> Build library"
 NODE_ENV=production node scripts/build.js
 
-echo "Update version.rb file"
+echo "> Update version.rb file"
 echo -e "module AblyUi\n  VERSION = '$RUBY_VERSION'\nend" > ./lib/ably_ui/version.rb
 
-echo "Build the gem"
+echo "> Build the gem"
 gem build ably-ui.gemspec
 
-echo "Push the gem to the registry"
+echo "> Push the gem to the registry"
 gem push ably-ui-$VERSION.gem
 
-echo "Remove local gem artifact"
+echo "> Remove local gem artifact"
 rm ably-ui-$RUBY_VERSION.gem
 
-echo "Update Gemfile.lock"
+echo "> Update Gemfile.lock"
 bundle
 
-echo "Publish the npm package to the registry"
+echo "> Publish the npm package to the registry"
 yarn publish --no-git-tag-version --new-version $VERSION
 
-echo "Update preview app version"
+echo "> Update preview app version"
 cd preview
 
-echo "Update Gemfile"
+echo "> Update Gemfile"
 # Using -i.bak is a cross-platform way of using sed
 # https://stackoverflow.com/a/22084103
 sed -i.bak "s/gem 'ably-ui', '.*', require/gem 'ably-ui', '${RUBY_VERSION}', require/" Gemfile
 rm Gemfile.bak
 
-echo "Update ably-ui npm package in preview app"
+echo "> Update ably-ui npm package in preview app"
 yarn upgrade @ably/ui@$VERSION
 
-echo "Update Gemfile.lock"
+echo "> Update Gemfile.lock"
 bundle lock  # don't change contents gem dir as it might be using local paths
 
-echo "Commit version publish and preview app update to $TAG"
+echo "> Commit version publish and preview app update to $TAG"
 cd ..
 git add package.json lib/ably_ui/version.rb Gemfile.lock
 git add preview/package.json preview/yarn.lock preview/Gemfile preview/Gemfile.lock
 git commit -m "Publish $TAG and update preview app"
 
-echo "Tag commit with $TAG"
+echo "> Tag commit with $TAG"
 git tag -a $TAG -m "$TAG"
 
-echo "Push to repo"
+echo "> Push to repo"
 git push origin HEAD
