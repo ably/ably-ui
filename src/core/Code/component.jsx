deb6a541bcea4b981be3c39bdf4e9ce2c05d7185
April Smith <aprilroseannkwong@gmail.com>
2022-03-14 13:56:29 +0000

    Customised padding and adding additional CSS params

--- src/core/Code/component.jsx
+++ src/core/Code/component.jsx
@@ -4,11 +4,11 @@ import T from "prop-types";
 import "./component.css";
 import { highlight } from "./component.js";
 
-const Code = ({ language, snippet, textSize = "ui-text-code1" }) => {
+const Code = ({ language, snippet, textSize = "ui-text-code1", padding = "p-32", additionalCSS = "" }) => {
   const HTMLraw = highlight(language, `${snippet}`.trim());
   const className = `language-${language} ${textSize}`;
   return (
-    <div className="hljs p-32 overflow-auto" data-id="code">
+    <div className={`hljs overflow-auto ${padding} ${additionalCSS}`} data-id="code">
       <pre lang={language}>
         <code className={className} dangerouslySetInnerHTML={{ __html: HTMLraw }} />
       </pre>
@@ -20,6 +20,8 @@ Code.propTypes = {
   language: T.string,
   snippet: T.string,
   textSize: T.string,
+  padding: T.string,
+  additionalCSS: T.string,
 };
 
 export default Code;
