5288267990040a922fd63ded5dd69f93e2e1cfde
Dominik Piatek <dominik.piatek@ably.io>
2022-04-07 14:47:15 +0100

    Add gui-success color

--- preview/app/components/colours_list_component.rb
+++ preview/app/components/colours_list_component.rb
@@ -4,27 +4,40 @@ class ColoursListComponent < ViewComponent::Base
       {
         group_name: "Primary",
         group_colours: [
-          { bg: 'bg-cool-black', name: "Cool Black", hex: '#03020D', rgb: '0, 2, 13' },
-          { bg: 'bg-active-orange', name: "Active Orange", hex: '#FF5416', rgb: '255, 84, 22' },
-          { bg: 'bg-white', name: "White", hex: '#FFFFFF', rgb: '255, 255, 255' }
+          { bg: 'bg-cool-black', name: "Cool Black", hex: '#03020D' },
+          { bg: 'bg-active-orange', name: "Active Orange", hex: '#FF5416' },
+          { bg: 'bg-white', name: "White", hex: '#FFFFFF' }
         ]
       },
       {
         group_name: "Secondary",
         group_colours: [
-          { bg: 'bg-electric-cyan', name: "Electric Cyan", hex: '#4AF7FF', rgb: '74, 247, 255' },
-          { bg: 'bg-zingy-green', name: "Zingy Green", hex: '#08FF13', rgb: '8, 255, 19' },
-          { bg: 'bg-bright-red', name: "Bright Red", hex: '#FF2739', rgb: '255, 39, 57' },
-          { bg: 'bg-jazzy-pink', name: "Jazzy Pink", hex: '#FF17D2', rgb: '255, 23, 210' }
+          { bg: 'bg-electric-cyan', name: "Electric Cyan", hex: '#4AF7FF' },
+          { bg: 'bg-zingy-green', name: "Zingy Green", hex: '#08FF13' },
+          { bg: 'bg-bright-red', name: "Bright Red", hex: '#FF2739' },
+          { bg: 'bg-jazzy-pink', name: "Jazzy Pink", hex: '#FF17D2' }
         ]
       },
       {
         group_name: "Neutrals",
         group_colours: [
-          { bg: 'bg-light-grey', name: "Light Grey", hex: '#F5F5F6', rgb: '245, 245, 246' },
-          { bg: 'bg-mid-grey', name: "Mid Grey", hex: '#D9D9DA', rgb: '217, 217, 218' },
-          { bg: 'bg-dark-grey', name: "Dark Grey", hex: '#76767C', rgb: '118, 118, 124' },
-          { bg: 'bg-charcoal-grey', name: "Charcoal Grey", hex: '#292831', rgb: '41, 40, 49' }
+          { bg: 'bg-light-grey', name: "Light Grey", hex: '#F5F5F6' },
+          { bg: 'bg-mid-grey', name: "Mid Grey", hex: '#D9D9DA' },
+          { bg: 'bg-dark-grey', name: "Dark Grey", hex: '#76767C' },
+          { bg: 'bg-charcoal-grey', name: "Charcoal Grey", hex: '#292831' }
+        ]
+      },
+      {
+        group_name: "GUI",
+        group_colours: [
+          { bg: 'bg-gui-success', name: "Success", hex: '#11CB24' },
+          { bg: 'bg-gui-error', name: "Error", hex: '#fb0c0c' },
+          { bg: 'bg-gui-default', name: "Default", hex: '#0073e6' },
+          { bg: 'bg-gui-hover', name: "Hover", hex: '#0867c4' },
+          { bg: 'bg-gui-focus', name: "Focus", hex: '#80b9f2' },
+          { bg: 'bg-gui-active', name: "Active", hex: '#074095' },
+          { bg: 'bg-gui-viewed', name: "Viewed", hex: '#4887c2' },
+          { bg: 'bg-gui-unavailable', name: "Unavailable", hex: '#a8a8a8' }
         ]
       }
     ]
