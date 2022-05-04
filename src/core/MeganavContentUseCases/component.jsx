6c4955877c4e5e5878cb25a862cd1115493f5f83
Dominik Piatek <dominik.piatek@ably.io>
2022-05-04 10:27:29 +0100

    Add Fintech to Use Cases panel and reorder items

--- src/core/MeganavContentUseCases/component.jsx
+++ src/core/MeganavContentUseCases/component.jsx
@@ -9,15 +9,33 @@ const MeganavContentUseCases = ({ absUrl }) => (
       </h3>
       <ul aria-labelledby="meganav-use-cases-panel-industry-use-cases">
         <li>
-          <a href={absUrl("/solutions/extend-kafka-to-the-edge")} className="ui-meganav-media group">
-            <p className="ui-meganav-media-heading">Extend Kafka to the edge</p>
-            <p className="ui-meganav-media-copy">Enhance and reliably expand Kafka&apos;s event streaming capabilities beyond your private network.</p>
-          </a>
-        </li>
-        <li>
           <a href={absUrl("/solutions/edtech")} className="ui-meganav-media group">
             <p className="ui-meganav-media-heading">EdTech</p>
             <p className="ui-meganav-media-copy">Deliver interactive learning experiences like multi-user classrooms with chat.</p>
+          </a>
+        </li>
+        <li>
+          <a href={absUrl("/solutions/virtual-events")} className="ui-meganav-media group">
+            <p className="ui-meganav-media-heading">Virtual Events</p>
+            <p className="ui-meganav-media-copy">Power engaging virtual events with interactive realtime features.</p>
+          </a>
+        </li>
+        <li>
+          <a href={absUrl("/solutions/sports-and-media")} className="ui-meganav-media group">
+            <p className="ui-meganav-media-heading">Sports & Media</p>
+            <p className="ui-meganav-media-copy">Deliver global realtime experiences to keep fans informed, engaged, entertained.</p>
+          </a>
+        </li>
+        <li>
+          <a href={absUrl("/solutions/fintech")} className="ui-meganav-media group">
+            <p className="ui-meganav-media-heading">Fintech</p>
+            <p className="ui-meganav-media-copy">Deliver fast, personalised fintech data in realtime to mobile & web customers.</p>
+          </a>
+        </li>
+        <li>
+          <a href={absUrl("/solutions/gaming")} className="ui-meganav-media group">
+            <p className="ui-meganav-media-heading">Gaming</p>
+            <p className="ui-meganav-media-copy">Power interactive gaming experiences that are wicked fast and utterly reliable.</p>
           </a>
         </li>
         <li>
@@ -26,6 +44,11 @@ const MeganavContentUseCases = ({ absUrl }) => (
             <p className="ui-meganav-media-copy">Power asset tracking, live transit updates, race-critical diagnostics, and more.</p>
           </a>
         </li>
+      </ul>
+    </div>
+
+    <div>
+      <ul className="md:mt-40">
         <li>
           <a href={absUrl("/solutions/b2b-platforms")} className="ui-meganav-media group">
             <p className="ui-meganav-media-heading">B2B Platforms</p>
@@ -39,38 +62,15 @@ const MeganavContentUseCases = ({ absUrl }) => (
           </a>
         </li>
         <li>
-          <a href={absUrl("/solutions/virtual-events")} className="ui-meganav-media group">
-            <p className="ui-meganav-media-heading">Virtual Events</p>
-            <p className="ui-meganav-media-copy">Power engaging virtual events with interactive realtime features.</p>
+          <a href={absUrl("/solutions/ecommerce-and-retail")} className="ui-meganav-media group">
+            <p className="ui-meganav-media-heading">eCommerce & Retail</p>
+            <p className="ui-meganav-media-copy">Enable realtime pricing, inventory, and transactions to enrich user experiences.</p>
           </a>
         </li>
-      </ul>
-    </div>
-
-    <div>
-      <ul className="md:mt-40">
         <li>
           <a href={absUrl("/solutions/iot-and-connected-devices")} className="ui-meganav-media group">
             <p className="ui-meganav-media-heading">IoT & Connected Devices</p>
             <p className="ui-meganav-media-copy">Monitor and control global IoT deployments of any kind in realtime.</p>
-          </a>
-        </li>
-        <li>
-          <a href={absUrl("/solutions/sports-and-media")} className="ui-meganav-media group">
-            <p className="ui-meganav-media-heading">Sports & Media</p>
-            <p className="ui-meganav-media-copy">Deliver global realtime experiences to keep fans informed, engaged, entertained.</p>
-          </a>
-        </li>
-        <li>
-          <a href={absUrl("/solutions/gaming")} className="ui-meganav-media group">
-            <p className="ui-meganav-media-heading">Gaming</p>
-            <p className="ui-meganav-media-copy">Power interactive gaming experiences that are wicked fast and utterly reliable.</p>
-          </a>
-        </li>
-        <li>
-          <a href={absUrl("/solutions/ecommerce-and-retail")} className="ui-meganav-media group">
-            <p className="ui-meganav-media-heading">eCommerce & Retail</p>
-            <p className="ui-meganav-media-copy">Enable realtime pricing, inventory, and transactions to enrich user experiences.</p>
           </a>
         </li>
         <li>
@@ -79,6 +79,12 @@ const MeganavContentUseCases = ({ absUrl }) => (
             <p className="ui-meganav-media-copy">
               Track assets in realtime with a solution optimized for last mile logistics, food delivery applications, and urban mobility services.
             </p>
+          </a>
+        </li>
+        <li>
+          <a href={absUrl("/solutions/extend-kafka-to-the-edge")} className="ui-meganav-media group">
+            <p className="ui-meganav-media-heading">Extend Kafka to the edge</p>
+            <p className="ui-meganav-media-copy">Enhance and reliably expand Kafka&apos;s event streaming capabilities beyond your private network.</p>
           </a>
         </li>
       </ul>
