32f2adb7c328a67e2dfd6ab66f35a56677413bf4
Dominik Piatek <dominik.piatek@ably.io>
2022-06-21 12:49:05 +0100

    Split release action
    This simplifies the release script so it does not update the preview app and try to run the update dependents scripts.

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 env
 
 VERSION=$1
-TAG=v$1
 
 echo "Install packages, making sure they are up to date"
 yarn --frozen-lockfile
@@ -47,27 +46,9 @@ rm ably-ui-$VERSION.gem
 echo "Publish the npm package to the registry"
 yarn publish --no-git-tag-version --new-version $VERSION
 
-echo "Waiting to make sure packages are available in registries ;("
-sleep 180
-
-echo "Update preview app version"
-cd preview
-
-echo "Update Gemfile"
-sed -i.bak "s/gem 'ably-ui', '.*', require/gem 'ably-ui', '${VERSION}', require/" Gemfile
-rm Gemfile.bak
-
-echo "Update ably-ui npm package in preview app"
-yarn upgrade @ably/ui@$VERSION
-
-echo "Update Gemfile.lock"
-bundle lock  # don't change contents gem dir as it might be using local paths
-
-echo "Commit version publish and preview app update to $TAG"
-cd ..
+echo "Commit version publish"
 git add package.json lib/ably_ui/version.rb Gemfile.lock
-git add preview/package.json preview/yarn.lock preview/Gemfile preview/Gemfile.lock
-git commit -m "Commit version bump & preview app update"
+git commit -m "Commit version bump to $VERSION"
 
 echo "Push main to origin"
 git push origin main
