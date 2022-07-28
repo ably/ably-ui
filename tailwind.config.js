0fda76c982caecb181a17bc5806e85d5865a832f
Dominik Piatek <dominik.piatek@ably.io>
2022-07-28 11:08:26 +0100

    Add 2px and 6px to sizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -114,7 +114,9 @@ module.exports = {
     spacing: {
       0: "var(--spacing-0)",
       1: "var(--spacing-1)",
+      2: "var(--spacing-2)",
       4: "var(--spacing-4)",
+      6: "var(--spacing-6)",
       8: "var(--spacing-8)",
       12: "var(--spacing-12)",
       14: "var(--spacing-14)",
