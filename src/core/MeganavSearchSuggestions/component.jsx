57032ca07f2b9f435049f40195453914e8711a1d
Dominik Piatek <dominik.piatek@ably.io>
2022-08-02 11:42:17 +0100

    Don't display suggestsions as links

--- src/core/MeganavSearchSuggestions/component.jsx
+++ src/core/MeganavSearchSuggestions/component.jsx
@@ -11,22 +11,22 @@ const MeganavSearchSuggestions = ({ absUrl, displaySupportLink }) => {
       <div className="flex justify-between items-center overflow-x-scroll">
         <ul className="flex">
           <li className="py-12 pr-8 flex-shrink-0">
-            <a href={absUrl("/docs/how-ably-works")} className="ui-text-p2 ui-link">
+            <a href={absUrl("/docs/how-ably-works")} className="ui-text-p2">
               How does Ably work?
             </a>
           </li>
           <li className="py-12 px-8 flex-shrink-0">
-            <a href={absUrl("/docs/quick-start-guide")} className="ui-text-p2 ui-link">
+            <a href={absUrl("/docs/quick-start-guide")} className="ui-text-p2">
               Quickstart guide
             </a>
           </li>
           <li className="py-12 px-8 flex-shrink-0">
-            <a href={absUrl("/docs/core-features/pubsub")} className="ui-text-p2 ui-link">
+            <a href={absUrl("/docs/core-features/pubsub")} className="ui-text-p2">
               Publish/Subscribe Messaging
             </a>
           </li>
           <li className="py-12 pl-8 flex-shrink-0">
-            <a href={absUrl("/platform")} className="ui-text-p2 ui-link">
+            <a href={absUrl("/platform")} className="ui-text-p2">
               Platform
             </a>
           </li>
