436e05f66bd94ad81d1895430a7fdc411c5eaaa4
Dominik Piatek <dominik.piatek@ably.io>
2022-08-04 10:00:40 +0100

    Add hover styling to search suggestions

--- src/core/MeganavSearchSuggestions/component.jsx
+++ src/core/MeganavSearchSuggestions/component.jsx
@@ -11,22 +11,22 @@ const MeganavSearchSuggestions = ({ absUrl, displaySupportLink }) => {
       <div className="flex justify-between items-center overflow-hidden">
         <ul className="flex transition-transform">
           <li className="py-12 pr-8 flex-shrink-0">
-            <a href={absUrl("/docs/how-ably-works")} className="ui-text-p2">
+            <a href={absUrl("/docs/how-ably-works")} className="ui-text-p2 hover:text-gui-hover active:text-gui-active focus:text-gui-focus">
               How does Ably work?
             </a>
           </li>
           <li className="py-12 px-8 flex-shrink-0">
-            <a href={absUrl("/docs/quick-start-guide")} className="ui-text-p2">
+            <a href={absUrl("/docs/quick-start-guide")} className="ui-text-p2 hover:text-gui-hover active:text-gui-active focus:text-gui-focus">
               Quickstart guide
             </a>
           </li>
           <li className="py-12 px-8 flex-shrink-0">
-            <a href={absUrl("/docs/core-features/pubsub")} className="ui-text-p2">
+            <a href={absUrl("/docs/core-features/pubsub")} className="ui-text-p2 hover:text-gui-hover active:text-gui-active focus:text-gui-focus">
               Publish/Subscribe Messaging
             </a>
           </li>
           <li className="py-12 pl-8 flex-shrink-0">
-            <a href={absUrl("/platform")} className="ui-text-p2">
+            <a href={absUrl("/platform")} className="ui-text-p2 hover:text-gui-hover active:text-gui-active focus:text-gui-focus">
               Platform
             </a>
           </li>
