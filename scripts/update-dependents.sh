1e6cf4077401ff73f6d779ebc81e40277d8edc53
Dominik Piatek <dominik.piatek@ably.io>
2022-08-05 16:03:43 +0100

    Fix update-dependents script

--- scripts/update-dependents.sh
+++ scripts/update-dependents.sh
@@ -2,19 +2,18 @@
 
 set -eou pipefail
 
-if [[ -z $REGISTRY_USERNAME || -z $REGISTRY_TOKEN ]]; then
-  echo "REGISTRY_USERNAME or REGISTRY_TOKEN are not set, cannot trigger github actions"
+if [[ -z $REGISTRY_TOKEN ]]; then
+  echo "REGISTRY_TOKEN are not set, cannot trigger github actions"
   exit 1
 fi
 
 echo "Triggering update action on ably/website ..."
 WEBSITE_RESULT=$(curl \
-    -sS \
-    -X POST \
-    -H "Accept: application/vnd.github.github.v3+json" \
-    -u "${REGISTRY_USERNAME}:${REGISTRY_TOKEN}" \
-    https://api.github.com/repos/ably/website/actions/workflows/update-ably-ui.yml/dispatches \
-    -d '{"ref": "main"}')
+-X POST \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: token ${REGISTRY_TOKEN}" \
+https://api.github.com/repos/ably/website/actions/workflows/update-ably-ui.yml/dispatches \
+-d '{"ref":"main"}')
 
 if [ -z "$WEBSITE_RESULT" ]; then
   echo "Workflow for website triggered"
@@ -27,12 +26,11 @@ echo ""
 
 echo "Triggering update action on ably/voltaire ..."
 VOLTAIRE_RESULT=$(curl \
-    -sS \
-    -X POST \
-    -H "Accept: application/vnd.github.github.v3+json" \
-    -u "${REGISTRY_USERNAME}:${REGISTRY_TOKEN}" \
-    https://api.github.com/repos/ably/voltaire/actions/workflows/update-ably-ui.yml/dispatches \
-    -d '{"ref": "main"}')
+-X POST \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: token ${REGISTRY_TOKEN}" \
+https://api.github.com/repos/ably/voltaire/actions/workflows/update-ably-ui.yml/dispatches \
+-d '{"ref":"main"}')
 
 if [ -z "$VOLTAIRE_RESULT" ]; then
   echo "Workflow for voltaire triggered"
