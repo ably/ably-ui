c542bcaffa4556cb7b66fce292bda5f48b5715de
Dominik Piatek <do.piatek@gmail.com>
2023-12-06 14:26:11 +0000

    Fix formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
   content: [
-      "./src/**/*.html.erb",
-      "./src/**/*.jsx",
-      "./src/**/*.js",
-      "./src/**/*.json",
+    "./src/**/*.html.erb",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+    "./src/**/*.json",
     "./src/**/*.css",
   ],
   safelist: [
-    "w-1/2", "w-1/3", "w-1/4", "w-1/5", "w-1/6",
+    "w-1/2",
+    "w-1/3",
+    "w-1/4",
+    "w-1/5",
+    "w-1/6",
     { pattern: /^hljs.*/ },
-    { pattern: /^ui-.*/ }
+    { pattern: /^ui-.*/ },
   ],
   theme: {
     screens: {
