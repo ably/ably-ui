f0438f95870633d0af1b31df1b20969d071bd378
Dominik Piatek <do.piatek@gmail.com>
2023-12-13 12:07:53 +0000

    Add storybook scaffold

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     "plugin:react/recommended",
     "prettier",
     "plugin:cypress/recommended",
+    "plugin:storybook/recommended"
   ],
   parserOptions: {
     ecmaFeatures: {
---
+++ .storybook/main.js
@@ -0,0 +1,60 @@
+const path = require("path");
+
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack",
+    {
+      name: "@storybook/addon-styling-webpack",
+
+      options: {
+        rules: [
+          {
+            test: /\.css$/,
+            sideEffects: true,
+            use: [
+              require.resolve("style-loader"),
+              {
+                loader: require.resolve("css-loader"),
+                options: {
+                  importLoaders: 1,
+                },
+              },
+              {
+                loader: require.resolve("postcss-loader"),
+                options: {
+                  postcssOptions: {
+                    config: path.resolve(
+                      __dirname,
+                      "postcss.storybook.config.js"
+                    ),
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  core: {
+    disableTelemetry: true,
+  },
+};
+export default config;
