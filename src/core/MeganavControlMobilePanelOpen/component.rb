4b25c23a2073b4fbdc6e41545cbc87e0c6b2eb16
Dominik Piatek <do.piatek@gmail.com>
2023-12-13 16:35:26 +0000

    Remove all vw components

--- src/core/MeganavControlMobilePanelOpen/component.rb
+++
@@ -1,9 +0,0 @@
-module AblyUi
-  module Core
-    class MeganavControlMobilePanelOpen < ViewComponent::Base
-      def initialize(aria_controls:)
-        @aria_controls = aria_controls
-      end
-    end
-  end
-end
