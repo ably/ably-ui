a30a9807f2cf19ad8a8f6023db24d69b7af501e7
Jamie Henson <jamie.henson@ably.com>
2024-03-15 16:49:25 +0000

    chore: remove Rails preview directory

--- preview/app/components/active_link_component.rb
+++
@@ -1,7 +0,0 @@
-class ActiveLinkComponent < ViewComponent::Base
-  def initialize(to:, classes: "", active_classes: "")
-    @to = to
-    @classes = classes
-    @active_classes = active_classes
-  end
-end
