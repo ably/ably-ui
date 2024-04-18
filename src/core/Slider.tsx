bc1b6fefbf79e84a6e451b45636d22023f025e72
Jamie Henson <jamie.henson@ably.com>
2024-04-18 17:00:34 +0100

    fix: remove explicit .tsx imports that break package

--- src/core/Slider.tsx
+++ src/core/Slider.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, ReactNode } from "react";
-import Icon from "../core/Icon.tsx";
+import Icon from "./Icon";
 import "./Slider/component.css";
 
 interface SliderProps {
