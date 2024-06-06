41ec9b10452167f145d436c1b48552d9df2580e0
Jamie Henson <jamie.henson@ably.com>
2024-06-06 17:46:55 +0100

    feat: remove Cypress tests in favour of Storybook's test-runner

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,24 +2,20 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-
   core: {
     disableTelemetry: true,
   },
-
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
-
   docs: {},
-
   typescript: {
     reactDocgen: "react-docgen-typescript",
   },
