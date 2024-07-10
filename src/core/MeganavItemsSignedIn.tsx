a0e1aeafb469cb8dca1fd8e648758500508ac690
April Smith <april.smith@ably.com>
2024-07-10 17:15:05 +0100

    Remove the Meganav Account Panel dropdown

--- src/core/MeganavItemsSignedIn.tsx
+++ src/core/MeganavItemsSignedIn.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import MeganavControl from "./MeganavControl";
-import SignOutLink from "./SignOutLink";
 import MeganavSearch from "./MeganavSearch";
 import { AbsUrl, MeganavSessionState, MeganavTheme } from "./Meganav";
 
@@ -22,13 +21,7 @@ const MeganavItemsSignedIn = ({
   theme,
   absUrl,
 }: MeganavItemsSignedIn) => {
-  const links = sessionState.account
-    ? Object.keys(sessionState.account.links).map(
-        (key) => sessionState.account.links[key],
-      )
-    : [];
   const accountName = truncate(sessionState.accountName, 20);
-  const preferredEmail = truncate(sessionState.preferredEmail, 20);
 
   return (
     <ul className="hidden md:flex items-center">
@@ -41,72 +34,6 @@ const MeganavItemsSignedIn = ({
         >
           {accountName}
         </MeganavControl>
-
-        <div
-          className="ui-meganav-panel-account invisible"
-          id="account-panel"
-          data-id="meganav-panel"
-        >
-          {/* Users exist without accounts in which case there is no links here */}
-          {sessionState.account && (
-            <>
-              <p className="ui-meganav-overline mt-16 mx-16">Your account</p>
-              <ul className="mb-16 mx-16">
-                {links.map((item) => (
-                  <li key={item.href}>
-                    <a
-                      className="ui-meganav-account-link"
-                      href={absUrl(item.href)}
-                    >
-                      {item.text}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-
-          <p className="ui-meganav-overline mx-16">{preferredEmail}</p>
-          <ul className="mb-8 mx-16">
-            <li>
-              <a
-                href={absUrl(sessionState.mySettings.href)}
-                className="ui-meganav-account-link"
-              >
-                {sessionState.mySettings.text}
-              </a>
-            </li>
-            {sessionState.myAccessTokens && (
-              <li>
-                <a
-                  href={absUrl(sessionState.myAccessTokens.href)}
-                  className="ui-meganav-account-link"
-                >
-                  {sessionState.myAccessTokens.text}
-                  <span className="ui-version-tag">preview</span>
-                </a>
-              </li>
-            )}
-          </ul>
-
-          <hr className="ui-meganav-hr mb-16" />
-
-          {sessionState.logOut && (
-            <div className="mb-16 px-16">
-              <SignOutLink absUrl={absUrl} {...sessionState.logOut}>
-                {({ text, href, onClick }) => (
-                  <a
-                    onClick={onClick}
-                    href={absUrl(href)}
-                    className="ui-meganav-account-link"
-                  >
-                    {text}
-                  </a>
-                )}
-              </SignOutLink>
-            </div>
-          )}
-        </div>
       </li>
 
       <li>
