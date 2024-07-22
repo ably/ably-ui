72a5840582af002116abf8266ed9f262528d42f7
Kenneth Kalmer <kenneth.kalmer@ably.com>
2024-07-23 00:02:13 +0100

    chore: use new Status component in footer

--- src/core/Footer/Footer.stories.tsx
+++ src/core/Footer/Footer.stories.tsx
@@ -1,3 +1,5 @@
+import { delay, http, HttpResponse } from "msw";
+
 import Footer from "../Footer";
 
 import ablyStack from "../images/ably-stack.svg";
@@ -6,11 +8,26 @@ import bestSupport from "../images/best-support-2023.svg";
 import fastestImplementation from "../images/fastest-implementation-2023.svg";
 import highestUserAdoption from "../images/highest-user-adoption-2023.svg";
 
+const statusUrl = "https://ntqy1wz94gjv.statuspage.io/api/v2/status.json";
+
 export default {
   title: "JS Components/Footer",
   component: Footer,
   parameters: {
     layout: "fullscreen",
+    msw: {
+      handlers: {
+        status: http.get(statusUrl, async () => {
+          await delay();
+
+          return HttpResponse.json({
+            status: {
+              indicator: "none",
+            },
+          });
+        }),
+      },
+    },
   },
   args: {
     paths: {
@@ -20,6 +37,7 @@ export default {
       fastestImplementation,
       highestUserAdoption,
     },
+    statusUrl: statusUrl,
   },
 };
 
