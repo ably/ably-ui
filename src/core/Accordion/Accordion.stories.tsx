de040b130b8dd99879a81a3ede3ba83a345c82f1
Jamie Henson <jamie.henson@ably.com>
2024-09-11 15:12:37 +0100

    feat: Revamped Accordion to include theming, dynamic sizing, icons

--- src/core/Accordion/Accordion.stories.tsx
+++ src/core/Accordion/Accordion.stories.tsx
@@ -1,39 +1,154 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Accordion, { AccordionProps } from "../Accordion";
+import { IconName } from "../Icon/types";
+import { accordionThemes } from "./types";
+
+const lorem = (
+  <p className="mb-16">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque
+    congue risus id lobortis. Vivamus blandit dolor at ultricies cursus.
+    Phasellus pharetra nunc erat, quis porttitor mauris faucibus in. Donec
+    feugiat dapibus orci et blandit. Duis eleifend accumsan est nec euismod.
+    Proin imperdiet malesuada lacus, a aliquam eros aliquet nec. Sed eu dolor
+    finibus, sodales nisl a, egestas mi. In semper interdum lacinia. Duis
+    malesuada diam quis purus blandit, sit amet imperdiet neque accumsan. Morbi
+    viverra vitae risus ut pellentesque. Praesent ac blandit augue. Aliquam
+    purus lectus, lacinia in semper vitae, dictum eu felis. Donec vel pulvinar
+    eros, id facilisis neque. Aenean odio arcu, accumsan vel est in, lobortis
+    rhoncus ligula. Pellentesque sit amet odio velit.
+  </p>
+);
+
+const data = [...Array(5)].map((_, i) => ({
+  name: `Item ${i + 1}`,
+  content: lorem,
+}));
+
+const dataWithIcons = data.map((datum) => ({
+  ...datum,
+  icon: "icon-gui-document-generic" as IconName,
+}));
+
+const longData = data.map((datum) => ({
+  ...datum,
+  content: (
+    <>
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+      {lorem}
+    </>
+  ),
+}));
+
+const AccordionPresentation = ({ data, options }: AccordionProps) => (
+  <div className="grid sm:grid-cols-3 gap-16 w-full">
+    {accordionThemes.map((theme) => (
+      <div key={theme}>
+        <p className="ui-text-p3 mb-16 text-center font-mono">{theme}</p>
+        <Accordion
+          data={data}
+          options={options}
+          theme={theme}
+          className="flex-1"
+        />
+      </div>
+    ))}
+  </div>
+);
 
 export default {
   title: "JS Components/Accordion",
   component: Accordion,
-  args: {
-    data: [
-      {
-        name: "Item 1",
-        content: "Content 1",
+};
+
+export const Default = { render: () => AccordionPresentation({ data }) };
+
+export const AutoClose = {
+  render: () =>
+    AccordionPresentation({
+      data,
+      options: { autoClose: true },
+    }),
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "Opening a section will close any other open sections. Set with `autoClose` on `options`.",
       },
-      {
-        name: "Item 2",
-        content: "Content 2",
-      },
-      {
-        name: "Item 3",
-        content: "Content 3",
-      },
-      {
-        name: "Item 4",
-        content: "Content 4",
-      },
-    ],
-    autoClose: false,
-    topBorder: false,
-    bottomBorder: false,
-    arrowIcon: false,
+    },
   },
 };
 
-export const Default = {};
+export const SelectableHeaders = {
+  render: () =>
+    AccordionPresentation({
+      data: dataWithIcons,
+      options: { selectable: true },
+    }),
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "Opening a section will apply styling to the opened section header. Row icons are disabled. Set with `selectable` on `options`.",
+      },
+    },
+  },
+};
 
-export const AutoClose = {
-  render: (args: AccordionProps) => (
-    <Accordion data={args.data} autoClose={true} />
+export const StickyHeaders = {
+  render: () => (
+    <div className="h-[400px]">
+      {AccordionPresentation({
+        data: data,
+        options: { sticky: true, defaultOpenIndexes: [0, 1, 2, 3] },
+      })}
+    </div>
   ),
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "Section headers will fix to the top of the container when scrolled. Set with `sticky` on `options`.",
+      },
+    },
+  },
+};
+
+export const WithDefaultOpenSections = {
+  render: () =>
+    AccordionPresentation({
+      data: data,
+      options: { defaultOpenIndexes: [1] },
+    }),
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "The sections that correspond to the supplied indexes will be open by default. (e.g. `[1]` will open the second section). Set with `defaultOpenIndexes` on `options`.",
+      },
+    },
+  },
+};
+
+export const WithRowIcons = {
+  render: () =>
+    AccordionPresentation({
+      data: dataWithIcons,
+    }),
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "When an icon name is supplied, it will be displayed to the left of the section name. Set with `icon` on `data` entries.",
+      },
+    },
+  },
+};
+
+export const LongContent = {
+  args: {
+    data: longData,
+  },
 };
