6e60f2dbe491582fead7b476b27cd7fa47a7bbb1
Jamie Henson <jamie.henson@ably.com>
2024-11-28 18:52:15 +0000

    feat: add Toggle component

---
+++ src/core/Toggle.tsx
@@ -0,0 +1,33 @@
+import * as Switch from "@radix-ui/react-switch";
+import clsx from "clsx";
+import React from "react";
+
+type ToggleProps = {
+  id: string;
+  label?: string;
+  className?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Toggle: React.FC<ToggleProps> = ({ id, label, className, ...props }) => {
+  return (
+    <div className="flex items-center">
+      <Switch.Root
+        className={clsx(
+          "p-0 h-32 w-[56px] bg-neutral-600 rounded-full relative inline-block transition-colors data-[disabled]:bg-gui-unavailable data-[disabled]:cursor-not-allowed data-[state=checked]:bg-orange-600 focus:outline-gui-focus",
+          className,
+        )}
+        id={id}
+        {...props}
+      >
+        <Switch.Thumb className="block h-[28px] w-[28px] bg-white data-[disabled]:bg-neutral-500 rounded-full drop-shadow-toggle translate-x-2 data-[state=checked]:translate-x-[26px] transition-transform" />
+      </Switch.Root>
+      {label ? (
+        <label className="ml-16" htmlFor={id}>
+          {label}
+        </label>
+      ) : null}
+    </div>
+  );
+};
+
+export default Toggle;
