beb8a832b7bd682829f0d5c1f378eba10d9eaf3a
Kenneth Kalmer <kenneth.kalmer@ably.com>
2025-01-15 23:57:52 +0000

    chore: expect Rails markup in notices

--- src/core/Notice.tsx
+++ src/core/Notice.tsx
@@ -11,6 +11,7 @@ type ContentWrapperProps = {
   children: ReactNode;
   textColor?: ColorClass | ColorThemeSet;
 };
+import useRailsUjsLinks from "./hooks/use-rails-ujs-hooks";
 
 // TODO(jamiehenson):
 // This type is a bit messed up currently due to the NoticeScripts import being interpreted as NoticeProps.
@@ -81,6 +82,8 @@ const Notice = ({
     ALLOWED_ATTR: ["href", "data-method", "rel"],
   });
 
+  const contentRef = useRailsUjsLinks();
+
   return (
     <div
       className={cn("ui-announcement", bgColor, textColor)}
@@ -91,6 +94,7 @@ const Notice = ({
         <ContentWrapper buttonLink={buttonLink ?? "#"}>
           <strong className="font-bold whitespace-nowrap pr-4">{title}</strong>
           <span
+            ref={contentRef}
             className="pr-4"
             dangerouslySetInnerHTML={{
               __html: safeContent,
