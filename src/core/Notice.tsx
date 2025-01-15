e0bcede732dd1c4f877dbd0b52a75aac1f6e416c
Kenneth Kalmer <kenneth.kalmer@ably.com>
2025-01-15 21:31:37 +0000

    fix: sanitize notice body before rendering

--- src/core/Notice.tsx
+++ src/core/Notice.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, useEffect } from "react";
+import DOMPurify from "dompurify";
 
 import { ColorClass, ColorThemeSet } from "./styles/colors/types";
 import Icon from "./Icon";
@@ -75,6 +76,11 @@ const Notice = ({
     });
   }, []);
 
+  const safeContent = DOMPurify.sanitize(bodyText, {
+    ALLOWED_TAGS: ["a"],
+    ALLOWED_ATTR: ["href", "data-method", "rel"],
+  });
+
   return (
     <div
       className={cn("ui-announcement", bgColor, textColor)}
@@ -84,7 +90,12 @@ const Notice = ({
       <div className="ui-grid-px py-16 max-w-screen-xl mx-auto flex items-start">
         <ContentWrapper buttonLink={buttonLink ?? "#"}>
           <strong className="font-bold whitespace-nowrap pr-4">{title}</strong>
-          <span className="pr-4">{bodyText}</span>
+          <span
+            className="pr-4"
+            dangerouslySetInnerHTML={{
+              __html: safeContent,
+            }}
+          ></span>
           {buttonLabel && (
             <span className="cursor-pointer whitespace-nowrap text-gui-blue-default-light dark:text-gui-blue-default-dark">
               {buttonLabel}
