1f44d59c26735519827d3d30a423da1e6f5e0516
April Smith <april.smith@ably.com>
2025-01-31 12:16:40 +0000

    wip: Meganav Panel

--- src/core/Meganav/Meganav.stories.tsx
+++ src/core/Meganav/Meganav.stories.tsx
@@ -1,17 +1,86 @@
-import React from "react";
-import { Meta, StoryFn } from "@storybook/react";
-import Meganav from "../Meganav";
+import React, { useState } from "react";
+import { Meta } from "@storybook/react";
+import Meganav from "./Meganav";
+import { MeganavPanelHighlight, MeganavPanel } from "./MeganavPanel";
+import ProductTile from "../ProductTile";
+import { ProductName, products } from "../ProductTile/data";
+import { productsMenu, solutionsHighlight, solutionsMenu } from "./data";
 
+const ProductsGrid = () => {
+  const [selectedProduct, setSelectedProduct] = useState<ProductName | null>(
+    null,
+  );
+  return (
+    <>
+      {Object.keys(products).map((product) => (
+        <ProductTile
+          name={product as ProductName}
+          key={product}
+          selected={selectedProduct === product}
+          onClick={() => setSelectedProduct(product as ProductName)}
+        />
+      ))}
+    </>
+  );
+};
 
 export default {
   title: "Components/Meganav",
   component: Meganav,
-  tags: ["!autodocs"],
+  tags: ["autodocs"],
 } as Meta;
 
-const Template: StoryFn = (args) => <Meganav {...args} />;
+export const Default = {
+  render: () => {
+    return <Meganav />;
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "",
+      },
+    },
+  },
+};
 
-export const Default = Template.bind({});
-Default.args = {
-  // Add any default args if needed
+export const ProductsPanel = {
+  render: () => {
+    return (
+      <div className="w-[816px]">
+        <MeganavPanel
+          panelLeft={<ProductsGrid />}
+          panelRightItems={productsMenu}
+          panelRightHeading="platform"
+        />
+      </div>
+    );
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "",
+      },
+    },
+  },
+};
+
+export const SolutionsPanel = {
+  render: () => {
+    return (
+      <div className="w-[816px] bg-neutral-100 dark:bg-neutral-1200 relative z-10">
+        <MeganavPanel
+          panelLeft={<MeganavPanelHighlight content={solutionsHighlight} />}
+          panelLeftClassName="bg-neutral-100 dark:bg-neutral-1200 p-24"
+          panelRightItems={solutionsMenu}
+        />
+      </div>
+    );
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: "",
+      },
+    },
+  },
 };
--- src/core/Meganav/data.tsx
+++ src/core/Meganav/data.tsx
@@ -1,28 +1,30 @@
 import ProductTile from "../ProductTile";
+import { ProductName, products } from "../ProductTile/data";
+import React from "react";
+import { IconName } from "../Icon/types";
+
+export type FlyoutPanelList = {
+  label: string;
+  icon: IconName;
+  link: string;
+};
+
+export type FlyoutPanelHighlight = {
+  heading: string;
+  content: string;
+  labelLink: string;
+  url: string;
+  image: string;
+};
 
 export const headerNav = [
   {
     name: "Products",
     content: (
       <div className="grid grid-cols-2 w-full">
-        <ProductTile className="bg-neutral-000" name="pubsub" key="pubsub" />
-        <ProductTile className="bg-neutral-000" name="chat" key="chat" />
-        <ProductTile
-          className="bg-neutral-000"
-          name="assetTracking"
-          key="assetTracking"
-        />
-        <ProductTile className="bg-neutral-000" name="spaces" key="spaces" />
-        <ProductTile
-          className="bg-neutral-000"
-          name="liveSync"
-          key="liveSync"
-        />
-        <ProductTile
-          className="bg-neutral-000"
-          name="liveObjects"
-          key="liveObjects"
-        />
+        {Object.keys(products).map((product) => (
+          <ProductTile key={product} name={product as ProductName} />
+        ))}
       </div>
     ),
     link: "",
@@ -31,4 +33,90 @@ export const headerNav = [
   { name: "Company", content: "Company content", link: "" },
   { name: "Pricing", content: "Pricing content", link: "" },
   { name: "Docs", content: "", link: "/docs" },
+];
+
+export const productsMenu: FlyoutPanelList[] = [
+  {
+    label: "Infrastructure",
+    icon: "icon-gui-globe-alt-outline",
+    link: "/infrastructure",
+  },
+  {
+    label: "Integrations",
+    icon: "icon-gui-puzzle-piece-outline",
+    link: "/integrations",
+  },
+  {
+    label: "SDKs",
+    icon: "icon-gui-cube-transparent-outline",
+    link: "/docs/sdks",
+  },
+  {
+    label: "Security & Compliance",
+    icon: "icon-gui-shield-check-outline",
+    link: "/security-and-compliance",
+  },
+];
+
+export const solutionsHighlight: FlyoutPanelHighlight = {
+  heading: "Fan Engagement",
+  content: "Capture the attention of millions of fans during live events.",
+  labelLink: "Learn more",
+  url: "/fan-engagement",
+  image: "",
+};
+
+export const solutionsMenu: FlyoutPanelList[] = [
+  {
+    label: "Biztech",
+    icon: "icon-gui-building-office-outline",
+    link: "/solutions/ecommerce-and-retail",
+  },
+  {
+    label: "Fintech",
+    icon: "icon-gui-currency-dollar-outline",
+    link: "/solutions/fintech",
+  },
+  {
+    label: "Healthcare",
+    icon: "icon-gui-heart-outline",
+    link: "/solutions/healthcare",
+  },
+  {
+    label: "EdTech",
+    icon: "icon-gui-academic-cap-outline",
+    link: "/solutions/edtech",
+  },
+];
+
+export const companyHighlight: FlyoutPanelHighlight = {
+  heading: "Leading the realtime revolution",
+  content:
+    "Hear from our founders about Ably’s ambitious plans to become the world’s definitive realtime platform.",
+  labelLink: "About Ably",
+  url: "/about",
+  image: "",
+};
+
+export const companyMenu: FlyoutPanelList[] = [
+  {
+    label: "Customer stories",
+    icon: "icon-gui-star-outline",
+    link: "/",
+  },
+  {
+    label: "Careers",
+    icon: "icon-gui-briefcase-outline",
+    link: "/case-studies",
+  },
+  {
+    label: "Blog",
+    icon: "icon-gui-light-bulb-outline",
+    link: "/blog",
+  },
+  {
+    label: "Contact us",
+    icon: "icon-gui-chat-bubble-bottom-center-text-outline",
+    link: "/contact",
+  },
 ];
