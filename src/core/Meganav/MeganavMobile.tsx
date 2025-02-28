13877bcf05ddd1c855b5bab7da6bb6245ea12788
April Smith <april.smith@ably.com>
2025-02-28 14:03:32 +0000

    wip: Meganav with props session

--- src/core/Meganav/MeganavMobile.tsx
+++ src/core/Meganav/MeganavMobile.tsx
@@ -14,7 +14,7 @@ export const MeganavMobile = ({
   return (
     <>
       <Accordion
-        theme={"transparent"}
+        theme="transparent"
         className="overflow-y-auto px-16 pt-16"
         data={mobileNavItems}
         icons={{
@@ -30,15 +30,12 @@ export const MeganavMobile = ({
         }}
       />
       {menuItemLinks && (
-        <div className="">
+        <div className="mb-12">
           {menuItemLinks.map((link) => (
             <a
               href={link.link}
               key={link.name}
-              className={cn(
-                "px-16 w-full block ui-text-p1 hover:bg-neutral-100 dark:hover:bg-neutral-1200 active:bg-neutral-200 dark:active:bg-neutral-1100",
-                menuItemClassname,
-              )}
+              className={cn("px-16 w-full block ui-text-p1", menuItemClassname)}
             >
               {link.name}
             </a>
