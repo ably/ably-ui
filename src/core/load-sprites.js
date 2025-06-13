2181c69a14d010a70666486f24d571ec7a7ba38a
Jamie Henson <jamie.henson@ably.com>
2025-06-13 13:32:35 +0100

    fix: allow keying for sprite divs to distinguish between chunks

--- src/core/load-sprites.js
+++ src/core/load-sprites.js
@@ -1,17 +1,19 @@
-export default (spritesUrl) => {
-  if (document.querySelector(".ably-sprites")) return;
+export default (spritesUrls) => {
+  spritesUrls.forEach((spritesUrl, index) => {
+    if (document.querySelector(`.ably-sprites-${index}`)) return;
 
-  fetch(spritesUrl)
-    .then((response) => response.text())
-    .then((image) => {
-      const container = document.createElement("div");
-      container.className = "ably-sprites";
-      container.style.opacity = 0;
-      container.style.position = "absolute";
-      container.style.width = "0";
-      container.style.height = "0";
-      container.innerHTML = image;
-      document.body.appendChild(container);
-    })
-    .catch((err) => console.error(err));
+    fetch(spritesUrl)
+      .then((response) => response.text())
+      .then((image) => {
+        const container = document.createElement("div");
+        container.className = `ably-sprites-${index}`;
+        container.style.opacity = 0;
+        container.style.position = "absolute";
+        container.style.width = "0";
+        container.style.height = "0";
+        container.innerHTML = image;
+        document.body.appendChild(container);
+      })
+      .catch((err) => console.error(err));
+  });
 };
