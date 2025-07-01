8adb95fdbaf5127b066df54f5a3cd2722db25bae
Jamie Henson <jamie.henson@ably.com>
2025-07-01 17:28:23 +0100

    fix: don't bump release package version manually

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,8 +12,5 @@ pnpm install --frozen-lockfile
 echo "Build library"
 NODE_ENV=production pnpm build
 
-echo "Update package.json version"
-pnpm version $VERSION --no-git-checks --no-git-tag-version
-
 echo "Publish the npm package to the registry"
 pnpm publish --no-git-checks
