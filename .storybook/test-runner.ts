8957ffba648b2bf9ce13a8391179b9a07cbc2ce9
Jamie Henson <jamie.henson@ably.com>
2025-09-09 16:50:56 +0100

    feat: migrate eslint 8 to oxlint 1

---
+++ .storybook/test-runner.ts
@@ -0,0 +1,30 @@
+import type { TestRunnerConfig } from "@storybook/test-runner";
+
+const flakyStories = [
+  "components-status",
+  "components-meganav",
+  "components-accordion",
+  "components-footer",
+  "components-tab-menu",
+  "components-header",
+  "features-pricing-cards",
+];
+
+const config: TestRunnerConfig = {
+  async postVisit(page, _context) {
+    const url = page.url();
+
+    if (flakyStories.some((story) => url.includes(story))) {
+      return;
+    }
+
+    const elementHandler = await page.$("#storybook-root");
+    const innerHTML = await elementHandler?.innerHTML();
+
+    // We don't need TS to make sense of this, it's a hook to a place where 'expect' makes sense
+    // @ts-ignore
+    expect(innerHTML).toMatchSnapshot();
+  },
+};
+
+export default config;
