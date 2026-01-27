a2b7373d2a0cbf2f4b2ed36ab1e87c1fab62a655
Lenka Kocisova <lenka.kocisova@ably.com>
2026-01-27 15:13:02 +0200

    Meganav notice updates

--- src/core/Notice.tsx
+++ src/core/Notice.tsx
@@ -45,7 +45,7 @@ const Notice = ({
   title,
   config,
   closeBtn,
-  bgColor = "bg-orange-100 dark:bg-orange-1100",
+  bgColor = "bg-gradient-to-b from-orange-100 to-orange-200 dark:from-orange-1100 dark:to-orange-1000",
   textColor = defaultTextColor,
   onClose,
 }: NoticeProps) => {
@@ -80,12 +80,10 @@ const Notice = ({
   return (
     <div
       className={cn(
-        "ui-announcement relative z-[60]",
+        "ui-announcement relative z-[60] px-3 pt-3",
         isClosing
           ? "ui-announcement-hidden max-h-0 -translate-y-full opacity-0 overflow-hidden"
           : "ui-announcement-visible",
-        bgColor,
-        textColor,
       )}
       data-id="ui-notice"
     >
@@ -133,6 +131,53 @@ const Notice = ({
             />
           </button>
         )}
+      </div>
+      <div
+        className={cn(
+          "rounded-lg overflow-hidden ring-1 ring-inset ring-orange-200 dark:ring-orange-1000",
+          bgColor,
+          textColor,
+        )}
+      >
+        <div className="ui-grid-px py-4 max-w-screen-xl mx-auto relative">
+          <div className="flex justify-center text-center px-10">
+            <div>
+              <strong className="font-bold whitespace-nowrap pr-1">
+                {title}
+              </strong>
+              <span
+                ref={contentRef}
+                className="pr-1"
+                dangerouslySetInnerHTML={{ __html: safeContent }}
+              />
+              {buttonLabel && (
+                <span className="cursor-pointer whitespace-nowrap font-bold underline underline-offset-4 decoration-1 text-neutral-1100 hover:text-neutral-1300 dark:text-neutral-200 dark:hover:text-neutral-000">
+                  {buttonLabel}
+                </span>
+              )}
+            </div>
+          </div>
+
+          {closeBtn && (
+            <button
+              type="button"
+              className="absolute right-4 top-1/2 -translate-y-1/2 h-6 w-6 border-none bg-none outline-none focus:outline-none focus:ring-0 focus:border-transparent"
+              onClick={() => {
+                setIsClosing(true);
+                setTimeout(() => {
+                  document.dispatchEvent(new CustomEvent("notice-closed"));
+                  onClose?.();
+                }, 300);
+              }}
+            >
+              <Icon
+                name="icon-gui-x-mark-outline"
+                size="1.5rem"
+                color={textColor}
+              />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
